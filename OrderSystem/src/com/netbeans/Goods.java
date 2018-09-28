42889d7d92472ff9fc539e22d1a096177ba6134a
8435464 <32984056+8435464@users.noreply.github.com>
2018-09-28 11:31:07 +0800

    MVP Codes

---
+++ OrderSystem/src/com/netbeans/Goods.java
@@ -0,0 +1,46 @@
+package com.netbeans;
+
+/**
+ * 
+ * @author Kevin Che
+ *
+ */
+public class Goods {
+	int id;//Product ID, primary key 商品id，主键
+	String name;//Product name商品名称
+	float price;//Product price价格
+	int quantity;//Left quantity剩余数量
+	int sale;//Sold quantity已售数量
+        
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getSale() {
+		return sale;
+	}
+	public void setSale(int sale) {
+		this.sale = sale;
+	}
+
+}
